2eda20860a3e89b9367d1d388385245a9c920057
Andrew Porter <andrew.porter@manchester.ac.uk>
2004-04-02 16:28:31 +0000

    new class to hold and handle job metadata

---
+++ include/jobmetadata.h
@@ -0,0 +1,61 @@
+/*----------------------------------------------------------------------------
+    Application Class for QT launcher GUI.
+    Header
+
+    (C)Copyright 2003 The University of Manchester, United Kingdom,
+    all rights reserved.
+
+    This software is produced by the Supercomputing, Visualization &
+    e-Science Group, Manchester Computing, the Victoria University of
+    Manchester as part of the RealityGrid project.
+
+    This software has been tested with care but is not guaranteed for
+    any particular purpose. Neither the copyright holder, nor the
+    University of Manchester offer any warranties or representations,
+    nor do they accept any liabilities with respect to this software.
+
+    This software must not be used for commercial gain without the
+    written permission of the authors.
+    
+    This software must not be redistributed without the written
+    permission of the authors.
+
+    Permission is granted to modify this software, provided any
+    modifications are made freely available to the original authors.
+ 
+    Supercomputing, Visualization & e-Science Group
+    Manchester Computing
+    University of Manchester
+    Manchester M13 9PL
+    
+    WWW:    http://www.sve.man.ac.uk  
+    email:  sve@man.ac.uk
+    Tel:    +44 161 275 6095
+    Fax:    +44 161 275 6800    
+
+    Initial version by: M Riding, 29.09.2003
+    
+---------------------------------------------------------------------------*/
+
+#ifndef JOBMETADATA_H
+#define JOBMETADATA_H
+
+#include "qstring.h"
+
+/**Holds the description of the job being launched
+  *@author Mark Riding
+  */
+
+class JobMetaData {
+public: 
+	JobMetaData();
+	~JobMetaData();
+
+	QString  mPersonLaunching;
+	QString  mOrganisation;
+	QString  mLaunchTime;
+	QString  mSoftwareDescription;
+	QString  mPurposeOfJob;
+};
+
+#endif
