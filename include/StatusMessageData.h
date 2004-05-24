1214ebc3ccd20a8ecb960e6165175eb12373269b
Andrew Porter <andrew.porter@manchester.ac.uk>
2004-05-24 15:14:45 +0000

    New class to hold data for custom 'status' message events.

---
+++ include/StatusMessageData.h
@@ -0,0 +1,49 @@
+/*----------------------------------------------------------------------------
+    Class for holding data for custom event for status messages.
+
+    (C)Copyright 2003 The University of Manchester, United Kingdom,
+    all rights reserved.
+
+    This software is produced by the Supercomputing, Visualization &
+    e-Science Group, Manchester Computing, the Victoria University of
+    Manchester as part of the RealityGrid project.
+
+    This software has been tested with care but is not guaranteed for
+    any particular purpose. Neither the copyright holder, nor the
+    University of Manchester offer any warranties or representations,
+    nor do they accept any liabilities with respect to this software.
+
+    This software must not be used for commercial gain without the
+    written permission of the authors.
+    
+    This software must not be redistributed without the written
+    permission of the authors.
+
+    Permission is granted to modify this software, provided any
+    modifications are made freely available to the original authors.
+ 
+    Supercomputing, Visualization & e-Science Group
+    Manchester Computing
+    University of Manchester
+    Manchester M13 9PL
+    
+    WWW:    http://www.sve.man.ac.uk  
+    email:  sve@man.ac.uk
+    Tel:    +44 161 275 6095
+    Fax:    +44 161 275 6800    
+
+    Initial version by: A Porter 19.05.2004
+    
+---------------------------------------------------------------------------*/
+
+class StatusMessageData {
+public:
+  StatusMessageData(const QString msg, const int timeout){
+
+    msgTxt = msg;
+    mTimeout = timeout;
+  }
+
+  QString msgTxt;
+  int     mTimeout;
+};
