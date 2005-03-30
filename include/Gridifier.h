1c2ee5250f0e7d6564fb341888dd7573211b8d7d
Andrew Porter <andrew.porter@manchester.ac.uk>
2005-03-30 14:38:13 +0000

    Additional markup for doxygen plus additional code for MetaSGSs

--- include/Gridifier.h
+++ include/Gridifier.h
@@ -37,6 +37,10 @@
     
 ---------------------------------------------------------------------------*/
 
+/** @file Gridifier.h
+    @brief Class for wrapping scripts for handling OGSA framework and
+           remote job launching */
+
 #ifndef _GRIDIFIER_H_
 #define _GRIDIFIER_H_
 
@@ -54,10 +58,17 @@ public:
   Gridifier();
   ~Gridifier();
 
-  QString getSGSFactories(const QString &topLevelRegistry, const QString &desiredContainer);
-  QString makeSGSFactory(const QString &container, const QString &topLevelRegistry);
+  QString getSGSFactories(const QString &topLevelRegistry, 
+			  const QString &desiredContainer,
+			  const QString &className);
+  QString makeSGSFactory(const QString &container, 
+			 const QString &topLevelRegistry,
+			 const QString &className);
   QString makeSimSGS(const QString &factory, const LauncherConfig &config);
   QString makeVizSGS(const QString &factory, const LauncherConfig &config);
+  QString makeMetaSGS(const QString &factory,
+		      const LauncherConfig &config,
+		      const QString &parentGSH);
   QString checkPointAndStop(const QString &sgsGSH);
   void getSGSies(const QString &topLevelRegistry, QTable *aGSHTagTable);
   
@@ -65,21 +76,29 @@ public:
   void launchSimScript(const QString &scriptConfigFileName, const LauncherConfig &config);
   void launchVizScript(const QString &scriptConfigFileName, const LauncherConfig &config);
   void launchArgonneViz(const LauncherConfig &config);
+  /** Prototype */
   void webServiceJobSubmit(const QString &scriptConfigFileName);
 
   void copyCheckPointFiles(const QString &host);
+  /** Not used */
   void gsiFtp(const QString &file, const QString &destination);
 
+  /** Supply a pointer to the application object for the component
+      to be launched */
   void setApplication(QApplication *);
+  /** Set the location of the wrapper scripts */
+  void setScriptsDirectory(const QString &dir);
 
 private:
   QProcess *getSGSiesProcess;
 
-  // Keep a reference to the sgs/tag listview
+  /** Keep a reference to the sgs/tag listview */
   QTable *mGSHTagTable;
 
   QApplication *mApplication;
-  
+  /** Location of the various wrapper scripts */
+  QString       mScriptsDir;
+
 public slots:
   void getSGSiesProcessEnded();
   void gsiFtpStdoutSlot();
