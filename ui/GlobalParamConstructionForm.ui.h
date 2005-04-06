d4192f0a327ef9608ce26434e9fc2fe6a0f765e6
Andrew Porter <andrew.porter@manchester.ac.uk>
2005-04-06 12:26:52 +0000

    global param construction dialog now works

--- ui/GlobalParamConstructionForm.ui.h
+++ ui/GlobalParamConstructionForm.ui.h
@@ -19,16 +19,32 @@ void GlobalParamConstructionForm::init()
 
 void GlobalParamConstructionForm::addGlobal_clicked()
 {
-  int numSel = paramListTable->numSelections();
-  cout << "We have " << numSel << " selections" <<endl;
+  unsigned int i;
+  unsigned int numSel;
 
   globalParamContentListBox->clear();
 
-  for(int i=0; i<numSel; i++){
+  numSel = selectionList.count();
+  for(i=0; i<numSel; i+=2){
+    QString aText = paramListTable->text(selectionList[i],
+					 selectionList[i+1]);
+    globalParamContentListBox->insertItem(aText);
+ }
+
+  numSel = paramListTable->numSelections();
+
+  for(i=0; i<numSel; i++){
     QTableSelection aSel = paramListTable->selection(i);
+
+    // Store the row and column of this selection so we have
+    // it available to us in createGlobal_clicked()
+    selectionList.append(aSel.anchorRow());
+    selectionList.append(aSel.anchorCol());
+
     QString aText = paramListTable->text(aSel.anchorRow(),aSel.anchorCol());
     globalParamContentListBox->insertItem(aText);
   }
+  paramListTable->clearSelection();
 }
 
 
@@ -43,20 +59,35 @@ void GlobalParamConstructionForm::createGlobal_clicked()
     // user entered nothing or pressed Cancel
     return;
   }
-  /*
-  QString doc("<MSGS:Coupling_config>\n<Global_param_list>\n");
-  doc.append("<Global_param name=\"" + gName + "\">\n")
-  unsigned int nRows = globalParamContentListBox->count();
-  for(unsigned int i=0; i<nRows; i++){
 
-    doc.append("    <Child_param id=\"$param_set[$i]\" "
-	       "label=\"" +  globalParamContentListBox->text(i) + "\"/>\n");
+  QString doc("<MSGS:Coupling_config>\n<Global_param_list>\n");
+  doc.append("<Global_param name=\"" + gName + "\">\n");
+  unsigned int nItems = selectionList.count();
+  for(unsigned int i=0; i<nItems; i+=2){
+
+    // Reconstruct the full parameter label (we removed the
+    // leading GSH ID to make it more user-friendly)
+    QString gsh = paramListTable->text(0, selectionList[i+1]);
+    QStringList bits = QStringList::split("/", gsh);
+    QString gshID = bits.last() + "/";
+
+    doc.append("    <Child_param id=\""+ gsh +
+	       "\" label=\""+ gshID +
+               paramListTable->text(selectionList[i],selectionList[i+1])+
+	       "\"/>\n");
+
+    // Update the table to replace the selected parameters' labels
+    // with the name of the new global
+    paramListTable->setText(selectionList[i],selectionList[i+1], gName);
   }
 
   doc.append("  </Global_param>\n"
 	     "</Global_param_list>\n</MSGS:Coupling_config>\n");
-  */
+
+  cout << doc << endl;
+  gridifier.setServiceData(QString("MetaSGS"), mParentGSH, doc);
   globalParamContentListBox->clear();
+  selectionList.clear();
 }
 
 
@@ -86,10 +117,11 @@ void GlobalParamConstructionForm::setConfig( LauncherConfig *aConfig )
 
 void GlobalParamConstructionForm::setParentGSH( const QString &aGSH )
 {
-  QString     configFileList;
-  QStringList result;
-  int         i;
-  
+  QString      configFileList;
+  QStringList  result;
+  unsigned int i;
+  QStringList::Iterator it;
+
   mParentGSH = aGSH;
 
   if(!mConfig){
@@ -99,8 +131,7 @@ void GlobalParamConstructionForm::setParentGSH( const QString &aGSH )
   }
 
   // Get param defs from service...
-  // ...the output will be a space-delimited list of 
-  // filenames
+  // ...the output will be a space-delimited list of filenames
   if(!mParentGSH.isEmpty()){
     gridifier.getCoupledParamDefs(mParentGSH, &configFileList);
   }
@@ -108,28 +139,36 @@ void GlobalParamConstructionForm::setParentGSH( const QString &aGSH )
     cout << "Have no GSH for parent service" << endl;
     return;
   }
+  result = QStringList::split(" ", configFileList);
 
   // clear out the table
-  for (int i=paramListTable->numRows(); i>0; i--){
+  for (i=(paramListTable->numRows()-1); i>-1; i--){
     paramListTable->removeRow(i);
   }
-  for (int i=paramListTable->numCols(); i>0; i--){
+  for (i=(paramListTable->numCols()-1); i>-1; i--){
     paramListTable->removeColumn(i);
   }
 
-  result = QStringList::split(" ", configFileList);
+  int count = 0;
+  // Count how many valid file names we have (& thus how
+  // many columns we need)
+  for (it = result.begin(); it != result.end(); ++it){
+    if((*it).stripWhiteSpace().isEmpty())continue;
+    ++count;
+  }
 
   // Insert the required number of columns in the table
-  paramListTable->insertColumns(0, result.count());
+  cout << "Inserting " << count << " columns" << endl;
+  paramListTable->insertColumns(0, count);
   // Make a hidden row to hold GSHs
-  paramListTable->insertRows(1,0);
-  //paramListTable->hideRow(0);
+  paramListTable->insertRows(0,1);
+  paramListTable->hideRow(0);
 
-  int count = 0;
+  count = 0;
   // Loop over each file
-  for (QStringList::Iterator it = result.begin(); it != result.end(); ++it){
+  for (it = result.begin(); it != result.end(); ++it){
 
-    if((*it).isEmpty())continue;
+    if((*it).stripWhiteSpace().isEmpty())continue;
 
     QString name = mConfig->mScriptsDirectory + "/" + *it;
     QFile file(name);
@@ -145,12 +184,15 @@ void GlobalParamConstructionForm::setParentGSH( const QString &aGSH )
       continue;
     }
     QString appName = *it;
+    // Get the application name by removing the stuff that is appended
+    // to it in order to form the filename
     int index = appName.find(QString("_metadata"));
-    appName.truncate(appName.length() - index);
+    appName.truncate(index);
 
     // Set the column header to be the name of the application
     paramListTable->horizontalHeader()->setLabel(count, appName );
 
+    // Pull out the GSH of the service that has these param_defs
     QDomNodeList list = doc.elementsByTagName(QString("Param_defs"));
     if(!list.item(0).isElement()){
       cout << "ARPDBG: not a QDomElement :-(" << endl;
@@ -162,15 +204,21 @@ void GlobalParamConstructionForm::setParentGSH( const QString &aGSH )
     list = doc.elementsByTagName(QString("Label"));
 
     // '-1' allows for first, hidden, row of GSH values
-    if( (list.count()-1) > paramListTable->numRows()){
+    if( list.count() > (paramListTable->numRows()-1)){
       paramListTable->insertRows(paramListTable->numRows(),
-				 (list.count() - 1 - paramListTable->numRows()));
+				 (list.count() + 1 - paramListTable->numRows()));
     }
 
-    for(i=1; i<list.count(); i++){
+    for(i=0; i<list.count(); i++){
       QString label = list.item(i).firstChild().nodeValue();
-      cout << "Param label = " << label << endl;
-      paramListTable->setText(i, count, label);
+      // Remove the 'GSH_ID/' bit from the beginning of each label
+      // to make it more user friendly.
+      // The '\'s have to appear twice in C++ code??
+      label.remove(QRegExp("^\\d*\\/"));
+
+      // Insert the label into the table - '+1' allows for initial
+      // (hidden) row holding the GSH
+      paramListTable->setText(i+1, count, label);
     }
 
     file.close();
